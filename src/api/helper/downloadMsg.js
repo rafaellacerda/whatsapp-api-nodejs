dc62c3e54c71d9370c7eeafe2d4f7cbb51973f3d
Rafael Lacerda <rafa_jnd@hotmail.com>
2024-06-19 23:25:54 -0300

    fix: Download document

--- src/api/helper/downloadMsg.js
+++ src/api/helper/downloadMsg.js
@@ -2,17 +2,12 @@ const { downloadContentFromMessage } = require('@whiskeysockets/baileys')
 
 module.exports = async function downloadMessage(msg, msgType) {
     let buffer = Buffer.from([])
-    // let base64 = ''
 
     try {
         const stream = await downloadContentFromMessage(msg, msgType)
         for await (const chunk of stream) {
             buffer = Buffer.concat([buffer, chunk])
         }
-
-        // console.log('buffer', buffer)
-
-        // base64 = Buffer.from(buffer).toString('base64')
     } catch {
         return console.log('error downloading file-message')
     }
